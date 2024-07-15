23170a8d6506c7ed66418a9b2ea7b69df08197f2
Fr4nci <francesco291104@gmail.com>
2024-07-15 18:22:59 +0200

    Some aethestic fix

--- particle_simulation.c
+++ particle_simulation.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <unistd.h> //this code is compiled from Ubuntu WSL so i can't use "windows.h"
 #include <time.h>
 #define NUM_PARTICLES 1
 
@@ -8,17 +8,17 @@
 typedef struct {
     float x;
     float y;
-} Vector2;
+} Vector2; //declaration a 2-D vector
 
 typedef struct {
     Vector2 position;
     Vector2 velocity;
     float mass;
-} Particle;
+} Particle; //declaration of the type particle
 
 Particle particles[NUM_PARTICLES];
 
-void InitializeParticles() {
+void InitializeParticles() { //Here i inizialize the position of the variable randomnly
     for (int i = 0; i < NUM_PARTICLES; ++i) {
         particles[i].position = (Vector2){(float)rand() / RAND_MAX * 100, (float)rand() / RAND_MAX * 100};
         particles[i].velocity = (Vector2){0, 0};
@@ -27,18 +27,18 @@ void InitializeParticles() {
     printf("Inizio\n");
 }
 
-void PrintParticles() {
+void PrintParticles() { //This function print me the position of the various particles
     for (int i = 0; i < NUM_PARTICLES; i++) {
         Particle *particle = &particles[i];
         printf("i-th particle [%i] (%.2f, %.2f)\n", i, particle->position.x, particle->position.y);
     }
 }
 
-Vector2 ComputeForce(Particle *particle) {
+Vector2 ComputeForce(Particle *particle) { //this function compute the force of gravity on a particle
     return (Vector2){0, particle->mass * -9.81};
 }
 
-void RunSimulation() {
+void RunSimulation() { //it really explicit of what this function does
     float TotalSimulationTime = 10;
     float currentTime = 0;
     float dt = 0.025;
