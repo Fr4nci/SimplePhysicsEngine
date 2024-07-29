d07258bf1da1d4ea1721c36884f7445ebdbda9e7
Fr4nci <francesco291104@gmail.com>
2024-07-29 13:00:31 +0200

    Update Introduzione.ipynb

--- lib/vector.c
+++ lib/vector.c
@@ -1,11 +1,20 @@
 #include "../header/vector.h"
-float CrossProduct(Vector2 v1, Vector2 v2){
+#include <math.h>
+/* float CrossProduct(Vector2 v1, Vector2 v2){
     return(v1.x * v2.y + v1.y * v2.y);
-}
+} */
 float DotProduct(Vector2 v1, Vector2 v2){
     return(v1.x * v2.x + v1.y * v2.y);
 }
 
 Vector2 SumVectors(Vector2 v1, Vector2 v2){
     return (Vector2){v1.x + v2.x, v1.y + v2.y};
+}
+
+Vector2 ScMultiplication(Vector2 v1, float a){
+    return (Vector2){a * v1.x, a * v1.y};
+}
+
+float norm(Vector2 v1){
+    return(sqrt(DotProduct(v1)));
 }
